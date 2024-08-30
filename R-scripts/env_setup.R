c7371a0b35be33578ef2c0a03754e088f57ceb17
vincent <hankggg2003xd@gmail.com>
2024-08-30 08:37:05 +0800

    chore: Update renv package installation repository URL

--- R-scripts/env_setup.R
+++ R-scripts/env_setup.R
@@ -1,9 +1,6 @@
 # isntall renv package if not installed
 install.packages("renv", repos = "https://cran.csie.ntu.edu.tw/")
 
-# Set renv to snapshot all packages
-renv::settings$snapshot.type("all")
-
 # Snapshot the current environment
 renv::snapshot()
 
